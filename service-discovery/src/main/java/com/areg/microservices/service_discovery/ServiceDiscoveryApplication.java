a9ff108369094c7ea9cda5b2ef33ceba3e840bdd
Areg Abgaryan <aabgaryan@vmware.com>
2024-06-10 15:11:19 +0400

    Add service discovery microservice skeleton, minor fixes

---
+++ service-discovery/src/main/java/com/areg/microservices/service_discovery/ServiceDiscoveryApplication.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2024 Areg Abgaryan
+ */
+
+package com.areg.microservices.service_discovery;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * Service Discovery Application
+ *
+ * @since 2024
+ * @version 1.0
+ * @author Areg Abgaryan
+ */
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.areg.microservices.service_discovery")
+@OpenAPIDefinition(
+        info = @Info(title = "Service Discovery Microservice", version = "1.0.0",
+                contact = @Contact(name = "Areg Abgaryan", email = "abgaryan.areg@gmail.com", url = "https://www.linkedin.com/in/abgaryan-areg/")))
+public class ServiceDiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
